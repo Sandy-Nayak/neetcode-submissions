3d1c4580e443af107418cfd643ae11f659387831
Sandy <nayaksandeepraj@gmail.com>
2026-06-10 08:36:26 +0530

    Update: remove-node-from-end-of-linked-list - submission-0

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.java
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.java
@@ -13,19 +13,21 @@ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+
         ListNode slow = dummy;
         ListNode fast = dummy;
-        for(int i = 0; i <= n; i++){
+
+        for(int i = 0; i < n; i++){
             fast = fast.next;
         }
-            while(fast != null){
-                slow = slow.next;
-                fast = fast.next;
-            }
 
-            slow.next = slow.next.next;
-        
+        while(fast != null && fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
 
-       return dummy.next;
+        slow.next = slow.next.next;
+
+        return dummy.next;
     }
 }
