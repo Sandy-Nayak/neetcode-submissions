93465fae290336649315695f7e0042974fe947f2
Sandy <nayaksandeepraj@gmail.com>
2026-06-10 10:46:43 +0530

    Update: longest-repeating-substring-with-replacement - submission-0

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -1,78 +1,23 @@
-
-/*
-Example Walkthrough
-s = AABABBA
-k = 1
-
-Window grows:
-
-A
-len=1
-maxFreq=1
-1-1=0
-
-Valid.
-
-AA
-len=2
-maxFreq=2
-2-2=0
-
-Valid.
-
-AAB
-len=3
-maxFreq=2
-3-2=1
-
-Valid.
-
-AABA
-len=4
-maxFreq=3
-4-3=1
-
-Valid.
-
-AABAB
-len=5
-maxFreq=3
-5-3=2
-
-Invalid (2 > k)
-
-Shrink from left.
-
-
-And that quantity is:
-
-window size - count of the most frequent character
-
-Once that exceeds k, the window is no longer feasible and must be shrunk.
-
-*/
 class Solution {
     public int characterReplacement(String s, int k) {
         int[] freq = new int[26];
-        int left = 0;
-        int maxLength = 0;
+        int maxLen = 0;
         int maxFreq = 0;
 
+        int left = 0;
         for(int right = 0; right < s.length(); right++){
-            char c = s.charAt(right);
-            freq[c - 'A']++;
+            char c  = s.charAt(right);
+            freq[c-'A']++;
 
-            maxFreq = Math.max(maxFreq , freq[c - 'A']);
+            maxFreq = Math.max(maxFreq, freq[c-'A']);
 
-            while((right - left + 1) - maxFreq > k){
+            while((right - left + 1) - maxFreq > k ){
                 freq[s.charAt(left)-'A']--;
                 left++;
             }
-
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
-        return maxLength;
-    
+        return maxLen;
     }
 }
